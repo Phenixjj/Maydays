6694b54645abe35aeb8ad93f546f0cbb99ce5890
Phenixjj <58008493+Phenixjj@users.noreply.github.com>
2024-04-30 16:58:02 +0200

    fix: little adjustements

--- maydays/docusaurus.config.js
+++ maydays/docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/maydays-social-card.jpg',
+      image: 'static/img/maydays-social-card.jpg',
       navbar: {
         title: 'May Days',
         logo: {
