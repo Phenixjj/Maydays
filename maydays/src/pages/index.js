6bf2b0f1a8aef512fe0117409895b7d1f9e3074f
Phenixjj <58008493+Phenixjj@users.noreply.github.com>
2024-04-30 16:48:42 +0200

    fix: little adjustements and social card 2

--- maydays/src/pages/index.js
+++ maydays/src/pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
   return (
     <Layout
       title={` ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="May Day's, One Days, One Project <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
