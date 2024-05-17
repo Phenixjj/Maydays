96bcc05d85dd965187f756d9e0351c4d3feac9d8
Phenixjj <58008493+Phenixjj@users.noreply.github.com>
2024-05-17 18:37:23 +0200

    add interview training bot project

---
+++ md-p-09_interview-training-bot/db/populate_database.py
@@ -0,0 +1,104 @@
+import argparse
+import os
+import sys
+import shutil
+from langchain.document_loaders.pdf import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.schema.document import Document
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from utils.embedding import get_embedding_function
+from langchain.vectorstores.chroma import Chroma
+
+
+CHROMA_PATH = "chroma"
+DATA_PATH = "data"
+
+
+def main():
+    #check if database shoulg be cleared (using the --clear flag)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reset", action="store_true", help="Reset the database.")
+    args = parser.parse_args()
+    if args.reset:
+        print("✨ Clearing database.")
+        clear_database()
+
+    #create the data store
+    documents = load_documents()
+    chunks = split_documents(documents)
+    add_to_chroma(chunks)
+
+
+def load_documents():
+    document_loader = PyPDFDirectoryLoader(DATA_PATH)
+    return document_loader.load()
+
+
+def split_documents(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 800,
+        chunk_overlap=80,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    return text_splitter.split_documents(documents)
+
+
+def add_to_chroma(chunks: list[Document]):
+    #Load the existing database
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=get_embedding_function())
+
+    # Calculate Page Ids.
+    chunks_with_ids = calculate_chunk_ids(chunks)
+
+    # Add or Update the documents.
+    existing_items = db.get(include=[])
+    existing_ids = set(existing_items["ids"])
+    print(f"✨ Found {len(existing_ids)} existing documents in database.")
+
+    #Only add documents that don't exist in the DB.
+    new_chunks = [chunk for chunk in chunks_with_ids if chunk.metadata["id"] not in existing_ids]
+
+    if len(new_chunks):
+        print(f"👉 Adding new documents: {len(new_chunks)}")
+        new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
+        db.add_documents(new_chunks, ids=new_chunk_ids)
+        db.persist()
+    else:
+        print("✅ No new documents to add")
+
+
+def calculate_chunk_ids(chunks):
+    # This will create IDs like "data/ducument_1.pdf:6:2"
+    # Page Source : Page Number : Chunk Index
+
+    last_page_id = None
+    current_chunk_index = 0
+
+    for chunk in chunks:
+        source = chunk.metadata.get("source")
+        page = chunk.metadata.get("page")
+        current_page_id = f"{source}:{page}"
+
+        # If the page Id is the same as the last one, increment the index
+        if current_page_id == last_page_id:
+            current_chunk_index += 1
+        else:
+            current_chunk_index = 0
+        
+        # Calculate the chunk ID
+        chunk_id = f"{current_page_id}:{current_chunk_index}"
+        last_page_id = current_page_id
+
+        # Add the ID to the metadata
+        chunk.metadata["id"] = chunk_id
+    return chunks
+
+
+def clear_database():
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+
+if __name__ == "__main__":
+    main()
