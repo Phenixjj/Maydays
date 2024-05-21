088ad5c35c2893116dba0f7b284381475f75d2c9
Phenixjj <58008493+Phenixjj@users.noreply.github.com>
2024-05-21 21:30:17 +0200

    add project J-11

--- md-p-11_Crew-AI-and-RAG/AIssistant/aissistant/utils/utils.py
+++ md-p-11_Crew-AI-and-RAG/AIssistant/aissistant/utils/utils.py
@@ -1,19 +1,33 @@
-import os
-import subprocess
+import pandas as pd
 
-def list_all_commands():
-    """
-    List all available commands
-    """
-    stream = os.popen('compgen -c')
-    output = stream.read()
-    commands = output.split('\n')
-    return commands
+def convert_date():
+    # Read the CSV file
+    df = pd.read_csv('./data/Airplane_Crashes_and_Fatalities_Since_1908.csv')
+
+    # Convert the 'Date' column to datetime
+    df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y')
+
+    # Save the dataframe back to CSV
+    df.to_csv('Airplane_Crashes_and_Fatalities_Since_1908.csv', index=False)
 
 
-def execute_command(command: str):
-    """
-    Execute a command
-    """
-    sp = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    sp.communicate()
+def add_year_col():
+
+    # Read the CSV file
+    df = pd.read_csv('./data/Airplane_Crashes_and_Fatalities_Since_1908.csv')
+
+    # Extract the year from the 'Date' column
+    df['Year'] = pd.to_datetime(df['Date']).dt.year
+
+    # Rearrange the columns to put 'Year' before 'Date'
+    cols = df.columns.tolist()
+    cols = cols[-1:] + cols[:-1]
+    df = df[cols]
+
+    # Save the dataframe back to CSV
+    df.to_csv('Airplane_Crashes_and_Fatalities_Since_1908.csv', index=False)
+
+
+if __name__ == '__main__':
+    # convert_date()
+    add_year_col()
