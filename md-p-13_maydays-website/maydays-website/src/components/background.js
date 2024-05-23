d46018a4f64f35ac27952659bc7978e0a690a19b
Phenixjj <58008493+Phenixjj@users.noreply.github.com>
2024-05-23 18:17:24 +0200

    WIP website maydays project

---
+++ md-p-13_maydays-website/maydays-website/src/components/background.js
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+// import backgroundHero from '../../public/static/img/Default_2_Betta_Siamese_Fishing_Fish_swimming_together_and_tur_0_return.jpg'
+import backgroundHero from '../../public/static/img/Background_x3.jpg'
+ 
+export default function Background({className}) {
+  return (
+    <Image
+      alt="betta fish swimming together"
+      src={backgroundHero}
+      placeholder="blur"
+      quality={100}
+      fill={false}
+      className={className}
+    />
+  )
+}
