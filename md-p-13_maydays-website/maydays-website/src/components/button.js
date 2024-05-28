9ef2aea72e716e7bb5e84e6127f188d1987a5c31
Phenixjj <58008493+Phenixjj@users.noreply.github.com>
2024-05-28 18:55:24 +0200

    fix css

--- md-p-13_maydays-website/maydays-website/src/components/button.js
+++ md-p-13_maydays-website/maydays-website/src/components/button.js
@@ -7,7 +7,7 @@ export default function Button({className, url, text}) {
         window.open(url, '_blank');
       };
     return (
-        <button className={cn("outline transition hover:bg-white text-white hover:text-black font-bold py-3 px-10 rounded-full", className)} onClick={handleClick}>
+        <button className={cn("outline hover:outline-none transition hover:bg-white text-white hover:text-black font-bold py-3 px-10 rounded-full", className)} onClick={handleClick}>
             {text}
         </button>
     )
